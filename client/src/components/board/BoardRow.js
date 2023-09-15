bdfa25f9a73bd621d2756c91cbc39b98693486f2
Shane Israel <shanemisrael@gmail.com>
2023-09-14 18:56:04 -0600

    css animations and some updates on my game idea, experimentation, etc

--- client/src/components/board/BoardRow.js
+++ client/src/components/board/BoardRow.js
@@ -2,8 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Grid, Sheet, styled } from '@mui/joy'
 
-const LetterHolder = styled(Sheet)(({ theme, active }) => ({
-  // backgroundColor: theme.palette.mode === 'dark' ? '#3D3D4F' : '#bdbdbd',
+const LetterHolder = styled(Sheet)(({ theme, active, highlight, highlightborder }) => ({
   ...theme.typography['body-sm'],
   display: 'flex',
   justifyContent: 'center',
@@ -12,43 +11,78 @@ const LetterHolder = styled(Sheet)(({ theme, active }) => ({
   border: active
     ? `2px solid ${theme.palette.mode === 'dark' ? '#fff' : 'black'}`
     : `2px solid ${theme.palette.mode === 'dark' ? '#616161' : '#B6B6B6'}`,
-  // color: theme.vars.palette.text.secondary,
+  borderColor: highlightborder || false,
   width: 60,
   height: 60,
   fontSize: '1.25em',
   fontWeight: 'bold',
 }))
 
-const BoardRow = ({ active, letters }) => {
+const HighlightLetterHolder = styled(Sheet)(({ theme, active }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#ff000026' : '#FF000057',
+  ...theme.typography['body-sm'],
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  borderRadius: 2,
+  border: active
+    ? `2px solid ${theme.palette.mode === 'dark' ? 'red' : 'red'}`
+    : `2px solid ${theme.palette.mode === 'dark' ? '#ff000026' : '#FF000057'}`,
+  width: 60,
+  height: 60,
+  fontSize: '1.25em',
+  fontWeight: 'bold',
+}))
+
+const BoardRow = ({ active, letters, highlightIndexes }) => {
   if (letters) {
     return (
       <Grid container gap={1}>
-        {letters.map((letter, index) => (
-          <LetterHolder
-            key={(letter ? letter : 'blank_') + index}
-            className={letter ? 'tilt-shake' : ''}
-            active={letter}
-          >
-            {letter}
-          </LetterHolder>
-        ))}
+        {letters.map((letter, index) =>
+          highlightIndexes.includes(index) ? (
+            <HighlightLetterHolder
+              key={(letter ? letter : 'blank_') + index}
+              className={'skew-shake-infinite'}
+              active={letter}
+              highlight={highlightIndexes.includes(index) ? '#ff000026' : 0}
+              highlightborder={highlightIndexes.includes(index) ? '#ff0000f2' : 0}
+            >
+              {letter}
+            </HighlightLetterHolder>
+          ) : (
+            <LetterHolder
+              key={(letter ? letter : 'blank_') + index}
+              className={letter ? 'tilt-shake' : ''}
+              active={letter}
+            >
+              {letter}
+            </LetterHolder>
+          ),
+        )}
       </Grid>
     )
   }
   return (
     <Grid container gap={1}>
-      <LetterHolder />
-      <LetterHolder />
-      <LetterHolder />
-      <LetterHolder />
-      <LetterHolder />
+      {letters.map((_, index) =>
+        highlightIndexes.includes(index) ? (
+          <HighlightLetterHolder key={'blank_' + index} className="tilt-shake-infinite" />
+        ) : (
+          <LetterHolder key={'blank_' + index} />
+        ),
+      )}
     </Grid>
   )
 }
 
 BoardRow.propTypes = {
   active: PropTypes.bool.isRequired,
-  letters: PropTypes.array,
+  letters: PropTypes.array.isRequired,
+  highlightIndexes: PropTypes.array,
+}
+
+BoardRow.defaultProps = {
+  highlightIndexes: [],
 }
 
 export default BoardRow
--- client/src/components/board/GameBoard.js
+++ client/src/components/board/GameBoard.js
@@ -1,11 +1,31 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Grid, Sheet } from '@mui/joy'
+import { Button, Grid, Sheet, Typography } from '@mui/joy'
 import BoardRow from './BoardRow'
 import { useTheme } from '@emotion/react'
 
-const GameBoard = ({ rows, activeRow, rowLetters }) => {
+const GameBoard = ({ hide, rows, activeRow, rowLetters, rowHighlights, onStart }) => {
   const theme = useTheme()
+
+  function getBoard() {
+    if (!hide)
+      return new Array(rows)
+        .fill()
+        .map((_, index) => (
+          <BoardRow
+            key={index}
+            active={activeRow === index}
+            letters={rowLetters[index]}
+            highlightIndexes={rowHighlights[index]}
+          />
+        ))
+    return (
+      <Button onClick={onStart} size="lg">
+        Begin Todays Puzzle
+      </Button>
+    )
+  }
+
   return (
     <Sheet
       variant="outlined"
@@ -13,14 +33,15 @@ const GameBoard = ({ rows, activeRow, rowLetters }) => {
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'center',
+        justifyContent: 'center',
         gap: 1,
         padding: 2,
+        width: (366 / 5) * rowLetters[0]?.length || 5,
+        height: (434 / 6) * rows,
         background: theme.palette.mode === 'dark' ? false : theme.palette.neutral[100],
       }}
     >
-      {new Array(rows).fill().map((_, index) => (
-        <BoardRow key={index} active={activeRow === index} letters={rowLetters[index]} />
-      ))}
+      {getBoard()}
     </Sheet>
   )
 }
@@ -29,12 +50,16 @@ GameBoard.propTypes = {
   rows: PropTypes.number,
   activeRow: PropTypes.number,
   rowLetters: PropTypes.array,
+  rowHighlights: PropTypes.array,
+  onStart: PropTypes.func,
 }
 
 GameBoard.defaultProps = {
   rows: 5,
   activeRow: 0,
   rowLetters: [],
+  rowHighlights: [],
+  onStart: () => {},
 }
 
 export default GameBoard
