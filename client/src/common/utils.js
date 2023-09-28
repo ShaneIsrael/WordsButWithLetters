8aeb981782a1e7db0a4933cf7fc6b6815d572dde
Shane Israel <shanemisrael@gmail.com>
2023-09-28 01:05:07 -0600

    fuck it, regex just to be safe

--- client/src/common/utils.js
+++ client/src/common/utils.js
@@ -61,4 +61,5 @@ export const setPuzzleProgress = (date, board, progress) => {
   )
 }
 
-export const getUTCDate = () => new Date().toLocaleString('fr-CA', { timeZone: 'America/Los_Angeles' }).split(' ')[0]
+export const getUTCDate = () =>
+  new Date().toLocaleString('fr-CA', { timeZone: 'America/Los_Angeles' }).match(/\d{4}-\d{2}-\d{2}/g)[0]
--- server/src/utils/index.js
+++ server/src/utils/index.js
@@ -2,5 +2,5 @@ const validator = require('email-validator')
 module.exports = {
   isValidEmail: (email) => validator.validate(email),
   getTodaysDate: () =>
-    new Date().toLocaleString('fr-CA', { timeZone: 'America/Los_Angeles' }).split(' ')[0].replace(',', ''),
+    new Date().toLocaleString('fr-CA', { timeZone: 'America/Los_Angeles' }).match(/\d{4}-\d{2}-\d{2}/g)[0],
 }
