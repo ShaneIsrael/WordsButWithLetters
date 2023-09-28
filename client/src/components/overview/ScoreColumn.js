05fef4ac8a6b0797534cbf8639086d5a85debeb4
Seth Welch <sethwelch85@gmail.com>
2023-09-28 00:48:55 -0700

    Adding more resizing and adding instruction modal

--- client/src/components/overview/ScoreColumn.js
+++ client/src/components/overview/ScoreColumn.js
@@ -5,10 +5,18 @@ import { Grid, Sheet, Typography, useTheme } from '@mui/joy'
 const ScoreColumn = ({ wordScores }) => {
   const theme = useTheme()
   return (
-    <Sheet sx={{ pt: 1, pb: 1 }}>
+    <Sheet
+      sx={{
+        p: 1,
+        minWidth: 20,
+        borderRight: `1px solid ${theme.palette.mode === 'dark' ? '#25252D' : '#D8D8DF'}`,
+        borderTop: `1px solid ${theme.palette.mode === 'dark' ? '#25252D' : '#D8D8DF'}`,
+        borderBottom: `1px solid ${theme.palette.mode === 'dark' ? '#25252D' : '#D8D8DF'}`,
+      }}
+    >
       <Grid container direction="column" height="100%" alignItems="center">
         {wordScores.slice(0, 6).map((score, index) => (
-          <Grid key={`${score || 0}_${index}`} container xs alignItems="center" width="100%" pl={1}>
+          <Grid key={`${score || 0}_${index}`} container xs alignItems="center" width="100%">
             <Typography level="h2" fontSize={24} textAlign="center">
               {score ? score.toLocaleString('en-US') : 0}
             </Typography>
--- client/src/components/wrappers/PageWrapper.js
+++ client/src/components/wrappers/PageWrapper.js
@@ -7,12 +7,16 @@ const PageWrapper = ({ children }) => {
       display="flex"
       flexDirection="column"
       alignItems="center"
+      justifyContent="center"
       padding={'40px 20px'}
       width="100%"
       height="calc(100vh - 50px)"
       sx={{
-        ['@media (max-width:1200px)']: { scale: '0.9' },
-        ['@media (max-width:800px)']: { scale: '0.7' },
+        ['@media (max-width:1000px) (max-height:1000px)']: { scale: '0.9' },
+        ['@media (max-width:800px), (max-height:900px)']: { scale: '0.8' },
+        ['@media (max-width:600px), (max-height:800px)']: { scale: '0.7' },
+        ['@media (max-width:400px), (max-height:700px)']: { scale: '0.6' },
+        ['@media (max-width:300px), (max-height:600px)']: { scale: '0.5' },
       }}
     >
       <Box sx={{ height: 'auto' }}>{children}</Box>
