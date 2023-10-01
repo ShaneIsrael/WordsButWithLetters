7c9721ed228969f8e3cd412dd95887e531270aa9
Shane Israel <shanemisrael@gmail.com>
2023-09-30 21:07:21 -0600

    created a proper email verification template, created a thumbnail image

--- server/src/services/EmailService.js
+++ server/src/services/EmailService.js
@@ -1,4 +1,7 @@
 const nodemailer = require('nodemailer')
+const fs = require('fs')
+const handlebars = require('handlebars')
+
 const isProduction = process.env.NODE_ENV === 'production'
 
 const transporter = nodemailer.createTransport({
@@ -11,22 +14,45 @@ const transporter = nodemailer.createTransport({
     }
 });
 
+const readHTMLFile = (path, callback) => {
+  fs.readFile(path, {encoding: 'utf-8'}, (err, html) => {
+    if (err) {
+        callback(err);                 
+    }
+    else {
+        callback(null, html);
+    }
+  });
+};
 
 const service = {}
 
 service.sendVerificationEmail = async (email, token, name) => {
-    if (isProduction) {
-        try {
-            await transporter.sendMail({
-                from: process.env.SMTP_EMAIL,
-                to: email,
-                subject: 'Account Verification',
-                text: `Hi ${name},\nThanks for creating a Words But With Letters account. Please click on the link below to verify your account.\n\n${process.env.DOMAIN}/#/verify/${email}/${token}`
-            })
-        } catch (err) {
-            throw err
-        }
-    }
+  if (isProduction) {
+    console.log(__dirname)
+    readHTMLFile(__dirname + '/../templates/EmailVerification.html', async (err, html) => {
+      if (err) {
+        console.log('error reading file', err)
+        return
+      }
+      const template = handlebars.compile(html);
+      const replacements = {
+        verification_link: `${process.env.DOMAIN}/#/verify/${email}/${token}`
+      };
+      const htmlToSend = template(replacements);
+      try {
+        await transporter.sendMail({
+          from: process.env.SMTP_EMAIL,
+          to: email,
+          subject: 'Account Verification',
+          html: htmlToSend
+      })
+      } catch (err) {
+        throw err
+      }
+    })
+
+  }
 }
 
 module.exports = service
