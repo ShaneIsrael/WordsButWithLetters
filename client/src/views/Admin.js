12466672840657c4a36447f54f1ee03cc32cb099
Seth Welch <sethwelch85@gmail.com>
2023-10-12 23:48:21 -0700

    Adding migration, checking for admin, updating visuals

--- client/src/views/Admin.js
+++ client/src/views/Admin.js
@@ -1,9 +1,27 @@
 import { useTheme } from '@emotion/react'
 import DeleteIcon from '@mui/icons-material/Delete'
-import { Box, Grid, IconButton, List, ListItem } from '@mui/joy'
+import { Box, Grid, IconButton, List, ListItem, Typography } from '@mui/joy'
 import React, { useEffect } from 'react'
 import Appbar from '../components/appbar/Appbar'
 import FeedbackService from '../services/FeedbackService'
+
+const Token = ({ sx, children }) => {
+  const theme = useTheme()
+
+  return (
+    <Box
+      sx={{
+        borderRadius: 1,
+        background: theme.palette.primary[600],
+        padding: '4px 8px',
+        width: 'fit-content',
+        ...sx,
+      }}
+    >
+      {children}
+    </Box>
+  )
+}
 
 function Admin() {
   const [feedback, setFeedback] = React.useState([])
@@ -12,6 +30,7 @@ function Admin() {
 
   const darkMode = theme.palette.mode === 'dark'
   const backgroundColor = darkMode ? 'primary.900' : 'primary.300'
+  const borderColor = darkMode ? 'white' : 'black'
 
   const fetchFeedback = async () => {
     try {
@@ -38,6 +57,11 @@ function Admin() {
     }
   }
 
+  const getDate = (val) => {
+    const date = new Date(Date.parse(val))
+    return date.toLocaleString()
+  }
+
   return (
     <>
       <Appbar hideInstructions />
@@ -50,14 +74,11 @@ function Admin() {
           alignItems: 'center',
         }}
       >
-        <Grid
-          container
-          sx={{ width: 1200, height: 800, borderRadius: 8, border: `1px solid ${theme.palette.neutral[100]}` }}
-        >
+        <Grid container sx={{ width: 1200, height: 800, borderRadius: 8, border: `2px solid ${borderColor}` }}>
           <Grid
             xs={3}
             sx={{
-              borderRight: '1px solid white',
+              borderRight: `2px solid ${borderColor}`,
               height: '100%',
               overflow: 'auto',
               padding: '8px 4px',
@@ -67,7 +88,7 @@ function Admin() {
           >
             <List>
               {feedback.map((f, i) => {
-                const date = new Date(Date.parse(f.createdAt))
+                const date = getDate(f.createdAt)
                 const selectedValue = f.id === selected.id
                 return (
                   <ListItem
@@ -78,20 +99,29 @@ function Admin() {
                       borderRadius: 4,
                       margin: '2px 4px',
                       background: selectedValue ? '#051423' : '',
+                      color: selectedValue ? 'white' : '',
                       ':hover': {
                         cursor: 'pointer',
                       },
                     }}
                     onClick={() => setSelected(f)}
                   >
-                    {date.toLocaleString()}
+                    {date}
                   </ListItem>
                 )
               })}
             </List>
           </Grid>
           <Grid xs={9} sx={{ position: 'relative' }}>
-            <Box sx={{ margin: 4 }}>{selected?.body}</Box>
+            <Box sx={{ m: 1 }}>
+              <Token sx={{ mb: 2, width: '100%', textAlign: 'center' }}>
+                <Typography level="h2" sx={{ fontWeight: 500, fontSize: 32, color: 'white' }}>
+                  Feedback
+                </Typography>
+              </Token>
+              <Box sx={{ m: 1 }}>{selected?.body}</Box>
+            </Box>
+
             <IconButton
               size="large"
               onClick={archiveFeedback}
@@ -105,7 +135,7 @@ function Admin() {
                 borderRadius: 20,
               }}
             >
-              <DeleteIcon />
+              <DeleteIcon sx={{ color: 'white' }} />
             </IconButton>
           </Grid>
         </Grid>
